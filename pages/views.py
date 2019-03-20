52f370cddf722ca1e2558a9deec6b71f1aaaa49e
mikehagquist <mikehagquist@gmail.com>
2019-03-19 23:15:50 -0400

    More changes.

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import NameForm
 
 
 class HomePageView(TemplateView):
@@ -11,3 +15,23 @@ class AboutPageView(TemplateView):
 
 class SugarcutzPageView(TemplateView):
     template_name = 'pages/sugarcutz.html'
+
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
+
